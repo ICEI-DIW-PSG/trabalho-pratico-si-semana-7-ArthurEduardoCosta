7ccd656cf26bb3c112ca7136b20a05f2b0b97634
ArthurEduardoCosta <aeocosta@sga.pucminas.br>
2025-10-05 19:43:43 -0300

    Montagem estrutura inicial do projeto

--- public/app.js
+++ public/app.js
@@ -1,1 +1,34 @@
-// 
+// ======== DADOS DOS ARTIGOS (JSON) ========
+var dados = [
+  {
+    "id": 1,
+    "titulo": "A Batcaverna",
+    "descricao": "Explore o esconderijo secreto do Cavaleiro das Trevas!",
+    "conteudo": "A Batcaverna é o quartel-general do Batman. Equipada com tecnologia, veículos e o Batcomputador, é o centro das operações do herói.",
+    "categoria": "Locais",
+    "autor": "Bruce Wayne",
+    "data": "2025-10-05",
+    "imagem": "https://static.wikia.nocookie.net/the-lego-batman-movie/images/a/a8/The_Batcave.png"
+  },
+  {
+    "id": 2,
+    "titulo": "Vilões em LEGO",
+    "descricao": "Conheça os maiores inimigos do Batman em versão LEGO.",
+    "conteudo": "Do Coringa à Arlequina, cada vilão LEGO tem sua própria personalidade e planos malucos para dominar Gotham City!",
+    "categoria": "Personagens",
+    "autor": "Comissário Gordon",
+    "data": "2025-10-04",
+    "imagem": "https://hype.my/wp-content/uploads/2017/01/batman-lego-movie.jpeg"
+  },
+  {
+    "id": 3,
+    "titulo": "Heróis Aliados",
+    "descricao": "Robin, Batgirl e a Liga da Justiça em versão LEGO!",
+    "conteudo": "Mesmo sendo um herói solitário, Batman conta com aliados incríveis como Robin, Batgirl e outros heróis do universo LEGO DC.",
+    "categoria": "Heróis",
+    "autor": "Alfred Pennyworth",
+    "data": "2025-10-03",
+    "imagem": "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABX22eQkAjt_KkCBVEc-lTf_b4g66felyflvkrF7HyqSe_SgCAiwZUV54L5-mnj7M7_BaJvH_dP0WVEb3Yo4wrOCOXM5EQAnC6woz.jpg?r=d3c"
+  }
+];
+
